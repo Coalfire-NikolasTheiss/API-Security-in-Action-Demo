8025116abefd8e3193a97b196ee9e913cfd6be1f
Nikolas Theiss <154381541+Coalfire-NikolasTheiss@users.noreply.github.com>
2024-12-01 15:24:30 -0600

    Update Main.java
    Updated to use Guava for rate-limiting.

--- src/main/java/com/manning/apisecurityinaction/Main.java
+++ src/main/java/com/manning/apisecurityinaction/Main.java
@@ -1,38 +1,93 @@
 package com.manning.apisecurityinaction;
-import com.manning.apisecurityinaction.controller.*;
-import org.dalesbred.Database;
-import org.h2.jdbcx.JdbcConnectionPool;
-import org.json.*;
 
 import java.nio.file.*;
+
+import com.google.common.util.concurrent.*;
+import com.manning.apisecurityinaction.controller.*;
+import org.dalesbred.Database;
+import org.dalesbred.result.EmptyResultException;
+import org.h2.jdbcx.JdbcConnectionPool;
+import org.json.*;
+import spark.*;
+
 import static spark.Spark.*;
+
 public class Main {
 
+    public static void main(String... args) throws Exception {
+        var datasource = JdbcConnectionPool.create(
+            "jdbc:h2:mem:natter", "natter", "password");
+        var database = Database.forDataSource(datasource);
+        createTables(database);
+        datasource = JdbcConnectionPool.create(
+            "jdbc:h2:mem:natter", "natter_api_user", "password");
 
-  public static void main(String... args) throws Exception {
-  port(8080);               // Use port 8080 (or another open port)
-  ipAddress("0.0.0.0");     // Bind to all network interfaces (for external access)
-  var datasource = JdbcConnectionPool.create(
-"jdbc:h2:mem:natter", "natter", "password"); var database = Database.forDataSource(datasource); createTables(database);
-var spaceController =
-    new SpaceController(database);
-post("/spaces",
-    spaceController::createSpace);
-get("/spaces/:spaceId",
-    spaceController::getSpace);
-after((request, response) -> {
-  response.type("application/json");
-});
-internalServerError(new JSONObject()
-      .put("error", "internal server error").toString());
-    notFound(new JSONObject()
-      .put("error", "not found").toString());
-}
+        database = Database.forDataSource(datasource);
+        var spaceController = new SpaceController(database);
 
-  private static void createTables(Database database)
-      throws Exception {
+        var rateLimiter = RateLimiter.create(2.0d); // Create rate-limiter.
+
+        before((request, response) -> {
+            if (!rateLimiter.tryAcquire()) {
+                System.out.println("Rate limit exceeded, sending 429 response.");
+                response.header("Retry-After", "2");
+                halt(429, "\nRate limit exceeded. Please try again later.");
+            }
+        });
+
+        before(((request, response) -> {
+            if (request.requestMethod().equals("POST") &&
+            !"application/json".equals(request.contentType())) {
+                halt(415, new JSONObject().put(
+                    "error", "Only application/json supported"
+                ).toString());
+            }
+        }));
+
+        afterAfter((request, response) -> {
+            response.type("application/json;charset=utf-8");
+            response.header("X-Content-Type-Options", "nosniff");
+            response.header("X-Frame-Options", "DENY");
+            response.header("X-XSS-Protection", "0");
+            response.header("Cache-Control", "no-store");
+            response.header("Content-Security-Policy",
+                    "default-src 'none'; frame-ancestors 'none'; sandbox");
+            response.header("Server", "");
+        });
+
+        post("/spaces", spaceController::createSpace);
+
+        // Additional REST endpoints not covered in the book:
+        post("/spaces/:spaceId/messages", spaceController::postMessage);
+        get("/spaces/:spaceId/messages/:msgId",
+            spaceController::readMessage);
+        get("/spaces/:spaceId/messages", spaceController::findMessages);
+
+        var moderatorController =
+            new ModeratorController(database);
+        delete("/spaces/:spaceId/messages/:msgId",
+            moderatorController::deletePost);
+
+        internalServerError(new JSONObject()
+            .put("error", "internal server error").toString());
+        notFound(new JSONObject()
+            .put("error", "not found").toString());
+
+        exception(IllegalArgumentException.class, Main::badRequest);
+        exception(JSONException.class, Main::badRequest);
+        exception(EmptyResultException.class,
+            (e, request, response) -> response.status(404));
+    }
+
+  private static void badRequest(Exception ex,
+      Request request, Response response) {
+    response.status(400);
+    response.body(new JSONObject().put("error", ex.getMessage()).toString());
+  }
+
+    private static void createTables(Database database) throws Exception {
         var path = Paths.get(
-            Main.class.getResource("/schema.sql").toURI());
+                Main.class.getResource("/schema.sql").toURI());
         database.update(Files.readString(path));
-      }
+    }
 }
